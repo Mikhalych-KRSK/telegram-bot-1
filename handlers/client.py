ed876c8c9098ae4ea83f7cdee142a5e2a163710a
mixa <Luhnikov2000@mail.ru>
2022-11-02 07:46:03 +0700

    Перезапуск

--- handlers/client.py
+++ handlers/client.py
@@ -16,7 +16,7 @@ async def process_start_command(message: types.Message):
 #@dp.message_handler(commands=['Кто я?'])
 async def process_question_command(message: types.Message):
     #await message.reply("Ответил в лс!")
-    await bot.send_message(message.from_user.id, "Ты лох)")
+    await bot.send_message(message.from_user.id, "Ты лучше всех 😘")
 
 
 async def send_cekrets_command(message: types.Message):
@@ -24,20 +24,33 @@ async def send_cekrets_command(message: types.Message):
     await bot.send_video(message.chat.id, open('VideoFile1.mp4', 'rb'))
     await bot.send_message(message.from_user.id, "Напиши: 'Да, конечно, 100%' или 'Нет'")
     
+async def send_cekrets_command_two(message: types.Message):
+    await bot.send_message(message.from_user.id, "А теперь надо тебе подумать...")
+    await bot.send_message(message.from_user.id, "Подсказака: подпишись на https://t.me/noisy_house_forever")
+    await bot.send_video(message.chat.id, open('VideoFile2.mp4', 'rb'))
+
+    
+    
+    
 async def invitation(message: types.Message):
     await bot.send_message(message.from_user.id, "https://t.me/+itMNDkjlKj1iZTdi")
     
 async def question_one(message: types.Message):
-    await message.answer("Хороший вопрос...")
+    await message.reply("Хороший вопрос...")
     
 async def question_two(message: types.Message):
-    await message.answer("У нас есть слова, которые не прошли цензуру: Путин, Z, Ян, Аниме. Список будет пополняться))")
+    await message.reply("У нас есть слова, которые не прошли цензуру: Путин, Z, Ян, Аниме. Список будет пополняться))")
+
+async def question_three(message: types.Message):
+    await bot.send_message(message.chat.id, "Всем привет, главный ИИ данного чата ожил")
 
 
 def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(process_question_command, lambda message: "Кто я?" in message.text) 
     dp.register_message_handler(process_start_command, commands=['start'])
     dp.register_message_handler(send_cekrets_command, lambda message: "Секретик" in message.text)
+    dp.register_message_handler(send_cekrets_command_two, lambda message: "Загадка" in message.text)
     dp.register_message_handler(invitation, lambda message: "Да, конечно, 100%" in message.text)
-    dp.register_message_handler(question_one, lambda message: "Как дела?" in message.text) 
+    dp.register_message_handler(question_one, lambda message: "Бот, как дела?" in message.text) 
     dp.register_message_handler(question_two, lambda message: "Какая цензура?" in message.text) 
+    dp.register_message_handler(question_three, lambda message: "?" in message.text)
--- keyboard/client_kb.py
+++ keyboard/client_kb.py
@@ -2,11 +2,12 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
 
 b1 = KeyboardButton("/start")
 b2 = KeyboardButton("Кто я?")
-b3 = KeyboardButton("Секретик")
+#b3 = KeyboardButton("Секретик")
+b4 = KeyboardButton("Загадка")
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)#, one_time_keyboard=True)
 
 #reply_markup=ReplyKeyboardRemove удаление клавиатуры
 
 #kb_client.add(b1).add(b2) или
-kb_client.row(b1, b2).add(b3)
+kb_client.row(b1, b2)#.add(b4)
